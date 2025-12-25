0ea259382b2ac1ed310b11f0a4abbcd913aa6707
AiniA-kr <youmsunwoo@gmail.com>
2025-12-25 15:34:35 -0500

    Set up main routes for users and lectures

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const userRoutes = require('./userRoutes');
+const lectureRoutes = require('./lectureRoutes');
 
+// 사용자 관련 라우트
+router.use('/users', userRoutes);
+
+// 강의 관련 라우트
+router.use('/lectures', lectureRoutes);
+
+module.exports = router;
